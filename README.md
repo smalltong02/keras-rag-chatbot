b9f0a6f8d8f86cd84ec2384900cbf56a89533134
smalltong02 <smalltong02@gmail.com>
2024-01-23 20:38:29 -0800

    commit demo code.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # keras-rag-chatbot
-A project written in the Rust language with the goal of offline load of small LLM Model, specifically RAG (Retrieval Augmented Generation) on mobile devices.
+
+![image](./img/screenshot.png)
+
+
+Please downloaded `openchat_3.5.Q4_K_M.gguf` and `tokenizer.json` from Huggingface: 
+
+```
+download TheBloke/openchat_3.5-GGUF openchat_3.5.Q4_K_M.gguf
+download openchat/openchat_3.5 tokenizer.json
+
+Copy them to the folder ./models/openchat-3.5-gguf/
+```
+The downloads locate at `~/.cache/huggingface/hub/`.
+
+and then 
+
+```
+cargo run --release
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello World!");
+#![allow(unused)]
+
+use std::sync::mpsc::channel;
+
+mod output_stream;
+mod llmengine;
+
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    let (sender, receiver) = channel::<String>();
+    let sender1 = sender.clone();
+
+    let _thread = std::thread::spawn(move || {
+        
+        if let Err(_) = llmengine::start_engine(ui_handle, receiver) {
+            // process before exit.
+        }
+    });
+
+    let ui_handle = ui.as_weak();
+    ui.on_send_ask_content(move |content| {
+        update_dialog(ui_handle.clone(), content.to_string());
+        match sender.send(content.to_string()) {
+            Ok(_) => println!("Message sent successfully"),
+            Err(e) => println!("Error sending message: {}", e),
+        }
+        //sender.send(content.to_string()).unwrap();
+    });
+
+    ui.window().on_close_requested(move || {
+        sender1.send("_exit_".to_string()).unwrap();
+        slint::CloseRequestResponse::HideWindow
+    });
+
+    ui.run()
+}
+
+fn update_dialog(ui_handle: slint::Weak<AppWindow>, msg: String) {
+    _ = slint::invoke_from_event_loop(move || {
+        let ui_handle = ui_handle.unwrap();
+        let old_content = ui_handle.get_dialog();
+        ui_handle.set_dialog(old_content + &msg + "\n");
+    });
+}
+
+fn update_dialog_without_ln(ui_handle: slint::Weak<AppWindow>, msg: String) {
+    _ = slint::invoke_from_event_loop(move || {
+        let ui_handle = ui_handle.unwrap();
+        let old_content = ui_handle.get_dialog();
+        ui_handle.set_dialog(old_content + &msg);
+    });
 }
